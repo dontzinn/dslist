2c71e9e6024fab14d186ac137fb87be32884df6e
dont <hardpvloksluck@yahoo.com.br>
2025-08-23 21:18:31 -0300

    fetch: GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -54,12 +54,12 @@ public class Game {
 		this.id = id;
 	}
 
-	public String getTittle() {
+	public String getTitle() {
 		return title;
 	}
 
-	public void setTittle(String tittle) {
-		this.title = tittle;
+	public void setTitle(String Title) {
+		this.title = title;
 	}
 
 	public Integer getYear() {
